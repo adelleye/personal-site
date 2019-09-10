c5df2ee6ce57b80f87bf896e7f02e69231a89fad
Tobi Adeleye <tobiadeleye@Oluwatobis-MacBook-Pro.local>
2019-09-10 11:29:28 -0400

    Added Margins

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,15 +10,38 @@ const IndexPage = props => (
   <Layout>
     <SEO title="Home" />
     
-    
+    <SectionDiv>
     <SectionTitle>My name is Tobi, a Toronto based web and user interface designer. </SectionTitle>
     <SectionBridge>I'm currently designing a new version of this website, so it could look a little different each time you visit over the next few weeks. In the meantime you can check out my clothing brand Severe Nature or some of my work </SectionBridge>
-    
+    </SectionDiv>
     <Link to="/page-2/">LinkedIn</Link> 
     <Link to="/page-2/">Github</Link>
 
   </Layout>
 )
+
+const SectionDiv = styled.div`
+max-width: 860px;
+height: 500px;
+margin-top: 190px;
+margin-left: 140px;
+margin-right: 140px;
+
+@media (max-width: 860px) {
+  max-width: 800px;
+  margin-top: 130px;
+  margin-left: 30px;
+  margin-right: 30px;
+
+    }
+@media (max-width: 640px) {
+  max-width: 580px;
+  margin-top: 130px;
+  margin-left: 30px;
+  margin-right: 30px;
+
+    }
+`
 
 const SectionBridge = styled.p`
    color: rgba(153,153,153,1.0);
@@ -26,6 +49,12 @@ const SectionBridge = styled.p`
     font-weight:500;
     margin: 0;
     line-height: 48px;
+
+    @media (max-width: 860px) {
+      font-size: 28px;
+        line-height: 34px;
+
+    }
  
     @media (max-width: 640px) {
         font-size: 22px;
