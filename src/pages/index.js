7b2d13545b5e7863c89f515bd09e6194f54a80b2
Tobi Adeleye <tobiadeleye@Oluwatobis-MacBook-Pro.local>
2019-09-18 09:36:51 -0400

    Design changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,24 +1,30 @@
 import React from "react"
 import { Link } from "gatsby"
-
 import Layout from "../components/layout"
-
 import SEO from "../components/seo"
 import styled , { keyframes }from "styled-components"
 
 const IndexPage = props => (
+
   <Layout>
     <SEO title="Home" />
-    
+    <FirstPageContainer>
     <SectionDiv>
-    <SectionTitle>My name is Tobi, a Toronto based web and user interface designer. </SectionTitle>
-    <SectionBridge>I'm currently designing a new version of this website, so it could look a little different each time you visit over the next few weeks. In the meantime you can check out my clothing brand Severe Nature or some of my work </SectionBridge>
-    
+    <SectionTitle> Introduction Note </SectionTitle>
+    <SectionBridge> My name is Tobi, a Toronto based web and user interface designer. I use this site to learn & try new techniques, so it could look a little different each time you visit. In the meantime you can check out my clothing brand <SevereNatureLink href="https://severenature.com" >Severe Nature</SevereNatureLink>. Professional links are below. </SectionBridge>
     <LinkedInLink href="https://www.linkedin.com/in/tobi-adeleye-18ab1b119/">LinkedIn</LinkedInLink> 
     <GithubLink href="https://github.com/adelleye">Github</GithubLink>
     </SectionDiv>
+
+    <RectangularBoxLinks>
+    <ThingsImInterestedIn> Things I'm interested in </ThingsImInterestedIn>
+    </RectangularBoxLinks>
+
+    </FirstPageContainer>
   </Layout>
 )
+
+
 //
 const MainTextAnimation = keyframes`
    0% {
@@ -31,27 +37,32 @@ const MainTextAnimation = keyframes`
   }
 `
 
+const FirstPageContainer= styled.div`
+display:grid;
+grid-template-columns: repeat(2,auto);
+`
+
+
+//First Half of First page
 const SectionDiv = styled.div`
-max-width: 860px;
+max-width: 492px;
 height: 500px;
 padding-top: 190px;
 margin-left: 140px;
-margin-right: 140px;
+margin-right: 30px;
 
 @media (max-width: 860px) {
-  max-width: 800px;
+  max-width: 430px;
   padding-top: 190px;
   margin-left: 30px;
   margin-right: 30px;
 
     }
 @media (max-width: 640px) {
-  max-width: 580px;
+  max-width: 320px;
   padding-top: 190px;
   margin-left: 30px;
   margin-right: 30px;
-  
-
     }
  
 `
@@ -59,10 +70,10 @@ margin-right: 140px;
 
 const SectionTitle = styled.p`
     color: rgba(41,42,44,1.0);
-    font-size: 40px;
+    font-size: 16px;
     font-weight:500;
-    margin-bottom:30px;
-    line-height: 48px;
+    margin-bottom:20px;
+    line-height: 24px;
     opacity: 0;
    animation: ${MainTextAnimation};
   animation-duration: 3s;
@@ -86,11 +97,11 @@ const SectionTitle = styled.p`
 
 
 const SectionBridge = styled.p`
-   color: rgba(153,153,153,1.0);
-    font-size: 40px;
-    font-weight:500;
+   color: rgba(0,0,0,1.0);
+    font-size: 16px;
+    font-weight:400;
     margin: 0;
-    line-height: 48px;
+    line-height: 24px;
     opacity: 0;
    animation: ${MainTextAnimation};
   animation-duration: 3s;
@@ -112,16 +123,44 @@ const SectionBridge = styled.p`
 `
 
 
+const SevereNatureLink = styled.a `
+font-size: 16px;
+text-decoration:underline;
+text-decoration-style:dotted;
+font-weight:400;
+
+transition: 1s;
+color:rgba(0,0,0, 1.0);
+display: inline-block;
+
+&:hover { 
+  color:rgba(0,145,255, 1.0);
+  transform: translateY(-3px);
+   }
+   @media (max-width: 860px) {
+        font-size: 17px;
+        line-height: 28px;
+        margin-top:30px;
+    }
+ 
+    @media (max-width: 640px) {
+        font-size: 15px;
+        line-height: 28px;
+        margin-top:30px;
+    }
+`
+
+
+
 const LinkedInLink = styled.a `
-font-size: 20px;
+font-size: 16px;
 text-decoration:none;
 font-weight:500;
 margin-top:40px;
 margin-left: 0px;
 transition: 1s;
-
 display: inline-block;
-color:rgba(0,145,255, 0.8);
+color:rgba(0,0,0, 1.0);
 
 &:hover { 
   color:rgba(0,145,255, 1.0);
@@ -144,13 +183,13 @@ color:rgba(0,145,255, 0.8);
 `
 
 const GithubLink = styled.a `
-font-size: 20px;
+font-size: 16px;
 text-decoration:none;
-font-weight:500;
+font-weight:400;
 margin-top:40px;
 margin-left: 20px;
 transition: 1s;
-color:rgba(0,145,255, 0.8);
+color:rgba(0,0,0, 1.0);
 display: inline-block;
 
 &:hover { 
@@ -169,10 +208,48 @@ display: inline-block;
         margin-top:30px;
     }
 
-
-
 `
 
+//Second Half of First page
+ const RectangularBoxLinks = styled.div`
+ max-width: 500px;
+height: 500px;
+padding-top: 190px;
+margin-left: 0px;
+margin-right: 140px;
+
+@media (max-width: 860px) {
+  max-width: 430px;
+  padding-top: 190px;
+  margin-left: 30px;
+  margin-right: 30px;
+
+    }
+@media (max-width: 640px) {
+  max-width: 320px;
+  padding-top: 190px;
+  margin-left: 30px;
+  margin-right: 30px;
+    }
+ `
+
+
+
+const ThingsImInterestedIn = styled.div`
+width: 250px;
+height: 300px;
+margin-left: 50px;
+margin-bottom: 50px;
+border-style: solid;
+transition: 1s;
+display:grid;
+justify-items: center;
+align-items: center;
+
+&:hover { 
+  box-shadow: 5px 5px rgba(0,0,0,0.5);
+   }
+`
 
 ;
 export default IndexPage
